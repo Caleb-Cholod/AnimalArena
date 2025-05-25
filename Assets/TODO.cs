e1a76558b074bc2f81be0a05325d27c9253b9a58
Caleb Cholod <caleb.cholod@maine.edu>
2025-05-25 13:39:16 -0400

    To Do list

---
+++ Assets/TODO.cs
@@ -0,0 +1,18 @@
+//TO DO LIST FOR PROGRAMMING
+//=========================
+//()Boss
+//()Shop Progress/UI
+//()Main Menu
+//()Enemy Spawn Waves and Complexity
+//()Enemy Randomized Stats
+//()Item Get Implementation
+//()Settings?
+//()Add in Items
+//()Lighting
+//()Shadow cast Script
+//()Gold and Health UI
+//()Death Screen and Player Taking Damage
+//()Add in Playjama SDK
+//()Extras, Screen Shake, Feedback, Hit Stun
+//()Mouse over UI Script
+//()SFX
